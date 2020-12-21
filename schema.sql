1bffc80a061e0c8ba596277773dfb6a59ccd11a2
Steven Crutcher <georgie.prescott@gmail.com>
2020-12-21 13:35:06 -0600

    schema 2

--- schema.sql
+++ schema.sql
@@ -1,54 +1,65 @@
--- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
-SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
-FROM employees AS e LEFT JOIN salaries AS s
-ON e.emp_no=s.emp_no
-ORDER BY e.emp_no;
+CREATE TABLE "departments" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
 
--- 2. List employees who were hired in 1986.
-SELECT * FROM employees
-WHERE DATE_PART('year',hire_date) = 1986
-ORDER BY emp_no;
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" CHAR(4)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
-SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
-FROM dept_manager AS dm
-INNER JOIN departments AS d
-	ON dm.dept_no=d.dept_no
-LEFT JOIN employees AS e
-	ON dm.emp_no=e.emp_no;
+CREATE TABLE "dept_manager" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees AS e
-INNER JOIN dept_emp AS de
-	ON e.emp_no=de.emp_no
-INNER JOIN departments AS d
-	ON de.dept_no=d.dept_no;
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "gender" CHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
 
--- 5. List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT * FROM employees
-WHERE first_name = 'Hercules' AND last_name like 'B%';
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees AS e
-LEFT JOIN dept_emp AS de
-	ON e.emp_no=de.emp_no
-INNER JOIN departments AS d
-	ON de.dept_no=d.dept_no
-WHERE d.dept_name='Sales';
+CREATE TABLE "titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR(100)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees AS e
-LEFT JOIN dept_emp AS de
-	ON e.emp_no=de.emp_no
-INNER JOIN departments AS d
-	ON de.dept_no=d.dept_no
-WHERE d.dept_name in ('Sales','Development');
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name, COUNT(*) AS freq_count
-FROM employees
-GROUP BY last_name
-ORDER BY freq_count DESC;
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
